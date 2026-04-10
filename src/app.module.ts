c0ac73e779845631ed90de96f213ec67d2eb4b58
Ahmad Amen Tazkrhge <tazkrhgeahmadamen@gmail.com>
2026-04-10 13:51:29 +0300

    clean AppModule a

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SlugController } from './slug/slug.controller';
-import { SlugService } from './slug/slug.service';
 import { SlugModule } from './slug/slug.module';
 
 @Module({
   imports: [SlugModule],
-  controllers: [AppController, SlugController],
-  providers: [AppService, SlugService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
